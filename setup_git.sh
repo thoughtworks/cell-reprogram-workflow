370af047f4dc258423f9295e8561f43e27d5c6c6
Avani7 <avani.mahadik@thouthworks.com>
2024-01-23 19:32:12 +0530

    create a single installable file

---
+++ setup_git.sh
@@ -0,0 +1,21 @@
+
+
+# dependencies for TransSynW
+
+git clone --depth=1 https://github.com/Avani7/Pipeline.git pipeline
+
+cd pipeline
+
+mkdir ./craft/dependencies
+cd ./craft/dependencies
+touch __init__.py
+git clone --depth=1 https://gitlab.lcsb.uni.lu/CBG/transsynw.git transsynw
+
+# dependencies for Signet
+wget https://github.com/Lan-lab/SIGNET/raw/main/SIGNET_Tutorial/SIGNET.py -O SIGNET.py
+
+cd -
+pip install .
+# if the user wants to download the latest data from TRRUST database
+# # curl -s 'https://www.grnpedia.org/trrust/data/trrust_rawdata.human.tsv' >> trrust_rawdata_human.tsv
+# echo "Setup completed"
