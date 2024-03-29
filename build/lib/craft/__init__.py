af9583ec22e5b3bd1b4a99f86c5de700a79604d3
Avani7 <avani.mahadik@thouthworks.com>
2024-03-29 15:29:02 +0530

    Renamed the tool

--- build/lib/craft/__init__.py
+++
@@ -1,2 +0,0 @@
-# from .tool import paga_tool, PAGA, signet_tool, transsynw_tool
-# from .stages import generate_hypothesis,mechanistic_insights,grn_inference,create_network,functional_analysis,trrust_analysis
